f5282a66441c3dfac359e5fa026464eb8a9611fc
Piotr Serafin <piotr.serafin.71@gmail.com>
2018-05-22 17:45:14 +0200

    Save/Restore main activity state

--- app/src/main/java/com/piotrserafin/popularmovies/utils/MovieSortType.java
+++ app/src/main/java/com/piotrserafin/popularmovies/utils/MovieSortType.java
@@ -1,7 +1,32 @@
 package com.piotrserafin.popularmovies.utils;
 
-public enum MovieSortType {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public enum MovieSortType implements Parcelable{
     MOST_POPULAR,
     TOP_RATED,
-    FAVORITES
+    FAVORITES;
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(ordinal());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<MovieSortType> CREATOR = new Creator<MovieSortType>() {
+        @Override
+        public MovieSortType createFromParcel(Parcel in) {
+            return MovieSortType.values()[in.readInt()];
+        }
+
+        @Override
+        public MovieSortType[] newArray(int size) {
+            return new MovieSortType[size];
+        }
+    };
 }
